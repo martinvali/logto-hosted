72a906b36864e03c7d40631f6a5e59b4ef0db803
Martin Väli <martin.vali@uptime.eu>
2024-08-15 14:58:38 +0300

    improvements

--- packages/connectors/connector-webeid/src/index.ts
+++ packages/connectors/connector-webeid/src/index.ts
@@ -31,14 +31,21 @@ import {
 // eslint-disable-next-line @silverhand/fp/no-let
 let authCodeRequest: AuthorizationCodeRequest;
 
-const getAuthorizationUri = async ({ state, redirectUri }) => {
-  const queryParameters = new URLSearchParams({
-    state,
-    redirect_uri: redirectUri
-  });
+
+const getAuthorizationUri =
+  (getConfig: GetConnectorConfig): GetAuthorizationUri =>
+  async (state, redirectUri) => {
+
+    console.log(state);
+    console.log(redirectUri);
+
+    const queryParameters = new URLSearchParams({
+      state,
+      redirect_uri: redirectUri
+    });
 
     return `https://payload-uptime-new.vercel.app/auth/id-card-login?callback=${queryParameters.toString()}`;
-  };
+  }
 
 const getAccessToken = async (config: AzureADConfig, code: string, redirectUri: string) => {
   /*
