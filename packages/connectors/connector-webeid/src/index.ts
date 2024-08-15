77e4ebd4f4d9ef0a7312121a935e25359488d38a
Martin Väli <martin.vali@uptime.eu>
2024-08-15 15:17:46 +0300

    add state and redirectUri destructuring

--- packages/connectors/connector-webeid/src/index.ts
+++ packages/connectors/connector-webeid/src/index.ts
@@ -34,7 +34,7 @@ let authCodeRequest: AuthorizationCodeRequest;
 
 const getAuthorizationUri =
   (getConfig: GetConnectorConfig): GetAuthorizationUri =>
-  async (state, redirectUri) => {
+  async ({state, redirectUri}) => {
 
     console.log(state);
     console.log(redirectUri);
